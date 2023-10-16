b1d8a2639a91e0daa11d50ac1cd7aff190a854e5
Maksymilian Rogacewicz <maksymilian.rogacewicz@gmail.com>
2023-10-16 21:32:41 +0200

    before trying to clip overlay

--- src/classes/GameState/helpers/render.ts
+++ src/classes/GameState/helpers/render.ts
@@ -113,10 +113,10 @@ export const renderFlashlight = (gameState: GameState) => {
     gameState.environment.drawables,
   );
 
-  // gameState.ctx.save();
+  gameState.ctx.save();
+
   // gameState.ctx.globalCompositeOperation = "destination-out";
 
-  // gameState.ctx.fillStyle = "rgba(255,251,174,1)";
   gameState.ctx.fillStyle = "rgba(255,251,174,0.9)";
   gameState.ctx.beginPath();
   gameState.ctx.moveTo(playerPosition.x, playerPosition.y);
@@ -129,12 +129,53 @@ export const renderFlashlight = (gameState: GameState) => {
   gameState.ctx.closePath();
   gameState.ctx.fill();
   // gameState.ctx.globalCompositeOperation = "source-over";
-  // gameState.ctx.restore();
+  gameState.ctx.restore();
 
-  // gameState.ctx.beginPath();
-  // for (let i = 0; i < flashlightPoints.length; i++) {
-  //   const { x, y } = flashlightPoints[i];
-  //   gameState.ctx.arc(x, y, 2, 0, Math.PI * 2);
-  // }
-  // gameState.ctx.stroke();
+  // drawClip(gameState.ctx);
 };
+
+export const drawClip = (ctx: CanvasRenderingContext2D) => {
+  const dim = 800;
+  const half = dim / 2;
+  ctx.fillRect(0, 0, dim, dim);
+  ctx.translate(half, half);
+
+  // Create a circular clipping path
+  ctx.beginPath();
+  ctx.arc(0, 0, half, 0, Math.PI * 2, true);
+  ctx.clip();
+
+  // draw background
+  const lingrad = ctx.createLinearGradient(0, -half, 0, half);
+  lingrad.addColorStop(0, "#232256");
+  lingrad.addColorStop(1, "#143778");
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(-half, -half, dim, dim);
+
+  // draw stars
+  for (let j = 1; j < 50; j++) {
+    ctx.save();
+    ctx.fillStyle = "#fff";
+    ctx.translate(half - Math.floor(Math.random() * dim), half - Math.floor(Math.random() * dim));
+    drawStar(ctx, Math.floor(Math.random() * 4) + 2);
+    ctx.restore();
+  }
+};
+
+function drawStar(ctx: CanvasRenderingContext2D, r: number) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.moveTo(r, 0);
+  for (let i = 0; i < 9; i++) {
+    ctx.rotate(Math.PI / 5);
+    if (i % 2 === 0) {
+      ctx.lineTo((r / 0.525731) * 0.200811, 0);
+    } else {
+      ctx.lineTo(r, 0);
+    }
+  }
+  ctx.closePath();
+  ctx.fill();
+  ctx.restore();
+}
